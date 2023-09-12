e53a86dfd8e1468a2645029e63e5361c427512fc
Abdelrahman <abdelrahmanreda748@gmail.com>
2023-09-12 17:31:09 +0300

    create custom text field

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/constants.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 32,
+          ),
+          CustomTextField(),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  TextField(
+      cursorColor: kPrimaryColor,
+
+      decoration: InputDecoration(
+        hintText: 'Title',
+        hintStyle: TextStyle(color: kPrimaryColor),
+        border: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+          borderRadius: BorderRadius.all(
+            Radius.circular(12.0),
+          ),
+        ),
+        enabledBorder:const  OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+          borderRadius: BorderRadius.all(
+            Radius.circular(12.0),
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: kPrimaryColor),
+          borderRadius:const  BorderRadius.all(
+            Radius.circular(12.0),
+          ),
+        ),
+      ),
+    );
   }
 }
