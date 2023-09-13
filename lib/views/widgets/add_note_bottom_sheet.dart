dd31907e9a1ffb40920fa9c6a9bb77f2d3b043a5
Abdelrahman <abdelrahmanreda748@gmail.com>
2023-09-13 23:54:58 +0300

    create add note form and put random color for each note

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,86 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:note_app/bloc/note_bloc.dart';
-import 'package:note_app/models/note_model.dart';
-import 'package:note_app/views/widgets/custom_button.dart';
-import 'package:note_app/views/widgets/custom_text_field.dart';
+import 'package:note_app/views/widgets/add_note_form.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 32),
-      child: SingleChildScrollView(
+    return Padding(
+      padding: EdgeInsets.only(
+          top: 20,
+          left: 16,
+          right: 16,
+          bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: const SingleChildScrollView(
         child: AddNoteForm(),
       ),
     );
   }
 }
 
-class AddNoteForm extends StatefulWidget {
-  const AddNoteForm({super.key});
-
-  @override
-  State<AddNoteForm> createState() => _AddNoteFormState();
-}
-
-class _AddNoteFormState extends State<AddNoteForm> {
-  final GlobalKey<FormState> formKey = GlobalKey();
-  AutovalidateMode autovalidate = AutovalidateMode.disabled;
-  String? title, subTitle;
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      autovalidateMode: autovalidate,
-      child: Column(
-        children: [
-          CustomTextField(
-              hintText: "title",
-              onSaved: (value) {
-                title = value;
-              }),
-          const SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            hintText: "Content",
-            onSaved: (value) {
-              subTitle = value;
-            },
-            maxLines: 5,
-          ),
-          const SizedBox(
-            height: 32,
-          ),
-          CustomButton(
-            onTap: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-                BlocProvider.of<NoteBloc>(context).add(
-                  AddNoteEvent(
-                    noteModel: NoteModel(
-                        title: title!,
-                        subTitle: subTitle!,
-                        date: '22,may 2023',
-                        color: 0xffffcc80),
-                  ),
-                );
-                Navigator.pop(context);
-              } else {
-                autovalidate = AutovalidateMode.always;
-                setState(() {});
-              }
-            },
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-        ],
-      ),
-    );
-  }
-}
